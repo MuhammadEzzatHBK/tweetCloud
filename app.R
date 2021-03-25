6c6aba091fbf771295424751bef74e2f26a8c098
Muhammad Ezzat <muhammadezzat316@gmail.com>
2021-03-25 07:57:49 +0200

    Updating the app.

--- app.R
+++ app.R
@@ -10,31 +10,40 @@ library(tidytext)
 library(plotrix)
 library(syuzhet)
 library(reshape2)
+library(waffle)
+library(emojifont)
 
-
-ui <- fluidPage( titlePanel("tweetCloud2"),theme = shinythemes::shinytheme('flatly'),
+ui <- fluidPage(titlePanel("TweetDisect"),theme = shinythemes::shinytheme('flatly'),
 sidebarLayout(
         sidebarPanel(
-                textInput('username','Enter a twitter username:','joerogan'),
-                uiOutput("github"),
-                uiOutput("linkedin"),
-                sliderInput("n","Querry Size (in tweets):",1,100,50),
+                textInput('username','Enter a twitter username:','johncena'),
+                sliderInput("n","Querry Size (in tweets):",1,100,75),
                 uiOutput("user_name"),
                 uiOutput("screen_name"),
                 uiOutput("location"),
                 uiOutput("followers_count"),
                 uiOutput("description"),
-                uiOutput("profile_url")),
-        
+                uiOutput("profile_url"),
+                HTML("<br>"),
+                img(src='logo.png', height = 140, width = 140,align='center'),
+                uiOutput("github"),
+                uiOutput("linkedin")),
+                
         mainPanel(
             tabsetPanel(
-                tabPanel('WordCloud',plotOutput('cloud')),
-                tabPanel("Tweets",tableOutput('tweets')),
-                tabPanel("Top15 Tweeted Words",plotlyOutput('top15',height = '800px')),
+                navbarMenu("Frequency Plots",
+                           tabPanel("Tweet Frequency",plotlyOutput('ts')),
+                           tabPanel("Top15 Tweeted Words",plotOutput('top15',height = '800px'))),
+                tabPanel("Mutualism",HTML("<b>Mutual/Disparate Relationships</b>"),plotOutput('relationships'),
+                         HTML("ie: This compares the proportion of followed people that follows the user back to the proprtion that doesn't.")),
                 navbarMenu("Sentiment Analysis",
-                           tabPanel("Intution Chart",plotOutput("sentiment_chart",width="75%")),
-                           tabPanel("Emotion Chart",plotOutput('emotions_plot',width="85%",height = '600px')),
-                           tabPanel("Comparison Cloud",plotOutput("comparison_cloud",width="70%",height = '600px')))
+                           tabPanel('WordCloud',HTML("<b>Word Cloud</b>"),plotOutput('cloud')),
+                           tabPanel("+/-",fluidRow(
+                             column(5,HTML("<b>Sentiments Intution</b>"),plotOutput("sentiment_chart",width="100%")),
+                             column(7,HTML("<b>Comparison Cloud</b>"),plotOutput("comparison_cloud",width="100%",height = '600px'))
+                           )),                  
+                           tabPanel("Emotion Chart",plotOutput('emotions_plot',width="85%",height = '600px')))
+
                 
             )
          
@@ -53,14 +62,14 @@ server <- function(input, output) {
     output$followers_count<- renderUI({tagList("Number of followers: ", user()$followers_count)})
     output$description<- renderUI({tagList("BIO: ", user()$description)})
     profile <- reactive({a(user()$profile_url,href=user()$profile_url)})
-    output$profile_url<- renderUI({tagList("Website:  ", profile())})
+    output$profile_url<- renderUI({tagList("External:  ", profile())})
     
     #Authentication
-    rtweet::create_token(app = '',
-                         consumer_key = '',
-                         consumer_secret = '',
-                         access_token = '',
-                         access_secret = '')
+    rtweet::create_token(app = 'tweetDisect',
+                         consumer_key = 'kI80LTECCHyvnXjd1sy60kjCN',
+                         consumer_secret = 'EbhlRW9dqgdkFsctId8SPftGl9P7twEGvxZPlHkrcmwDm2kOkN',
+                         access_token = '1287092359159742464-ojLHr9ys9g37nezcAwZnU61JyXbIin',
+                         access_secret = '836abw9cVMiz6PrCO29nD90I6RkZqXP7YxBJiRsWaxxNN')
     
     #Extract Tweets
     timeline <- reactive({rtweet::get_timeline(input$username,n=input$n)})
@@ -86,26 +95,26 @@ server <- function(input, output) {
                                 min.freq = 1,max.words=200, random.order=FALSE, rot.per=0.35, 
                                 colors=brewer.pal(8, "Dark2"))})
     output$cloud <- renderPlot({word_cloud()},height = 700, width = 600 )
-    output$tweets <- renderTable({tweets()[1:input$n]})
-    
+
     #Top15Words BarPlot
-    top15 <- reactive({ggplotly(ggplot(frequency_array()[1:15,],
+    top15 <- reactive({ggplot(frequency_array()[1:15,],
                                           aes(x=reorder(frequency_array()$word[1:15],
                                           frequency_array()$freq[1:15]),y=frequency_array()$freq[1:15]))+
-                                          geom_col()+theme_hc()+coord_flip()+ggtitle('Top15 Tweeted Words')+
-                                          xlab('Word')+ylab("Frequency"))})
-    output$top15 <- renderPlotly(top15())
+                                          geom_col()+theme_hc()+coord_flip()+ggtitle(paste(
+                                          "Top 15 Tweeted Words by",user()$name))+
+                                          xlab('Word')+ylab("Frequency")})
+    output$top15 <- renderPlot(top15())
     
     #Sentiment Analysis
       #Inutition Chart
     text = reactive({paste(tweets(), collapse = ' ')})
     tokens <- reactive({data_frame(text = text()) %>% unnest_tokens(word, text)})
     sentiment_table <- reactive({tokens() %>%
-            inner_join(get_sentiments("bing")) %>%
-            count(sentiment)})
+        inner_join(get_sentiments("bing")) %>%
+        dplyr::count(sentiment)})
     output$sentiment_table <- renderTable(sentiment_table())
     sentiment_chart <- reactive({pie3D(sentiment_table()$n,labels = c("Negative","Positive"),
-                                     explode=0.075,main = "Sentiments Intuition")})
+                                       explode=0,col = c('firebrick2','seagreen2'))})
     output$sentiment_chart = renderPlot(sentiment_chart())
     
       #Emotion Chart
@@ -120,10 +129,46 @@ server <- function(input, output) {
     
       #Comparison Cloud
     comparison_cloud <-reactive({tokens() %>%inner_join(get_sentiments("bing")) %>%
-                       count(word, sentiment, sort = TRUE) %>% acast(word ~ sentiment, value.var = "n", fill = 0) %>%
-                       comparison.cloud(colors = c("dark red", "dark green"), title.size =2)}) 
+                       dplyr::count(word, sentiment, sort = TRUE) %>% acast(word ~ sentiment, value.var = "n", fill = 0) %>%
+                       comparison.cloud(colors = c("firebrick2", "seagreen4"), title.size =2)}) 
     output$comparison_cloud <- renderPlot(comparison_cloud())
     
+    #Mutual/Disaprate ratio
+    followers <- reactive({rtweet::get_followers(input$username)})
+    friends <- reactive({rtweet::get_friends(input$username)})
+    common <- reactive({length(intersect(followers()$user_id, friends()$user_id))})
+    uncommon <- reactive({length(setdiff(followers()$user_id, friends()$user_id))})
+    relationships <- reactive({c(`Mutual` = as.integer(common()),`Disparate`=as.integer(uncommon()))})
+    relationships_chart <- reactive({waffle(relationships()/20,colors = c('seagreen4','firebrick2'))})
+    output$relationships <- renderPlot(relationships_chart())
+    
+    #TimeSeries
+    ts <- reactive({ggplotly(rtweet::ts_plot(rtweet::get_timeline(input$username),"month")+theme_minimal()+xlab('Month')+ylab('Number Of Tweets')+
+                               ggtitle("Tweet Frequency by months"))})
+    output$ts <- renderPlotly(ts())
+    
+
+
 }
 
 shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
